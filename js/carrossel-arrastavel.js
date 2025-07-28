121b7b77fd13ced26e7452cdab694cbea4c56874
gustavo.albonico <gustavoalbonicogoncalves@hotmail.com>
2025-07-28 13:24:04 -0300

    feat - adicionando verificacao para ativar/destivar botoes

--- js/carrossel-arrastavel.js
+++ js/carrossel-arrastavel.js
@@ -6,6 +6,34 @@ let posicaoInicialEixoX;
 let posicaoScrollAtual;
 let animandoItem = false;
 
+const verificarBotoes = (carrosselLista, carroselItemTranslateXAtual, carrosselItensGap) => {
+    const carrosselItens = carrosselLista.find('.carrossel-customizado-item');
+    const carroselItemLargura = carrosselItens.first().width();
+
+    const carrosselListaRect = carrosselLista.get(0).getBoundingClientRect();
+    const carroselItemPrimeiroRect = carrosselItens.first().get(0).getBoundingClientRect();
+    const carroselItemUltimoRect = carrosselItens.last().get(0).getBoundingClientRect();
+
+    if (
+        carroselItemTranslateXAtual > 0 &&
+        Math.abs(carroselItemPrimeiroRect.left) < (carroselItemLargura + carrosselItensGap) - carrosselListaRect.left
+    ) {
+        carrosselLista.siblings('.carrossel-customizado-btn-anterior:not(.carrossel-customizado-btn-disabled):not(.infinito)').addClass('carrossel-customizado-btn-disabled');
+    } else {
+        carrosselLista.siblings('.carrossel-customizado-btn-anterior.carrossel-customizado-btn-disabled:not(.infinito)').removeClass('carrossel-customizado-btn-disabled');
+    }
+
+    if (
+        carroselItemTranslateXAtual < 0 &&
+        Math.abs(carrosselListaRect.right - carroselItemUltimoRect.right) < (carroselItemLargura + carrosselItensGap)
+    ) {
+        carrosselLista.siblings('.carrossel-customizado-btn-proximo:not(.carrossel-customizado-btn-disabled):not(.infinito)').addClass('carrossel-customizado-btn-disabled');
+    } else {
+        carrosselLista.siblings('.carrossel-customizado-btn-proximo.carrossel-customizado-btn-disabled:not(.infinito)').removeClass('carrossel-customizado-btn-disabled');
+    }
+
+
+}
 const validarLimitesArrastando = (carrosselLista, carrosselItens) => {
     const carroselItemLargura = carrosselItens.first().width();
     const carrosselListaRect = carrosselLista.get(0).getBoundingClientRect();
@@ -22,46 +50,28 @@ const validarLimitesArrastando = (carrosselLista, carrosselItens) => {
     return true;
 }
 
-carrosseisArrastaveisLista.on('mousedown touchstart', function (evento) {
-    if (evento.target.classList.contains('carrossel-customizado-lista')) return;
-    if (animandoItem) return;
-    arrastando = true;
-
-    const carrosselLista = $(evento.currentTarget);
-    carrosselLista.next('.carrossel-customizado-btn-proximo.arrastavel').addClass('arrastando');
-    carrosselLista.find('.carrossel-customizado-item').css('transition', 'none');
-
-    posicaoInicialEixoX = (evento.pageX || evento.originalEvent.touches[0].pageX) - carrosselLista.offset().left;
-    posicaoScrollAtual = carrosselLista.scrollLeft();
-});
-
-$(document).on('mouseup touchend', function (evento) {
-    if (!arrastando) return;
-    arrastando = false;
-    animandoItem = true;
-
-    const containerAtual = $(evento.currentTarget).find('.carrossel-customizado-btn-proximo.arrastavel.arrastando').closest('.carrossel-customizado-container');
+const arrastarCarroselFinitoItem = (containerAtual) => {
     const carrosselLista = containerAtual.find('.carrossel-customizado-lista');
     const carrosselItens = carrosselLista.find('.carrossel-customizado-item');
 
     const carrosselItensGap = parseInt(containerAtual.css('--carrossel-itens-gap'));
     const carroselItemLargura = carrosselItens.first().width();
     const carroselItemTranslateXAtual = +carrosselItens.first().css('transform').match(/matrix\(([^)]+)\)/)[1].split(', ')[4];
-    const carroselQuantidadeItensArrastado = Math.ceil(Math.abs(carroselItemTranslateXAtual) / carroselItemLargura);
+    const carroselQuantidadeItensArrastado = Math.ceil(Math.abs(carroselItemTranslateXAtual) / (carroselItemLargura + carrosselItensGap));
+    const limiteArrastandoAtingido = +!validarLimitesArrastando(carrosselLista, carrosselItens);//ele retorna boolean sendo true 1 false 0
+    const carroselItemTranslateXAjuste = (carroselQuantidadeItensArrastado - limiteArrastandoAtingido) * (carrosselItensGap + carroselItemLargura);
 
-    let limiteArrastandoAtingido = !validarLimitesArrastando(carrosselLista, carrosselItens);
-    let carroselItemTranslateXAjuste = ((carrosselItensGap * carroselQuantidadeItensArrastado) + (carroselItemLargura + (carroselItemTranslateXAtual % carroselItemLargura)));
-    let carroselItemTranslateXAtualizado = carroselItemTranslateXAtual + (-1 * carroselItemTranslateXAjuste);
-    carroselItemTranslateXAtualizado = limiteArrastandoAtingido ? carroselItemTranslateXAtualizado + (carroselItemLargura + carrosselItensGap) : carroselItemTranslateXAtualizado;
+    let carroselItemTranslateXAtualizado = -1 * carroselItemTranslateXAjuste;
     if (carroselItemTranslateXAtual > 0) {
-        carroselItemTranslateXAjuste = (carrosselItensGap * carroselQuantidadeItensArrastado) + (carroselItemLargura - (carroselItemTranslateXAtual % carroselItemLargura));
-        carroselItemTranslateXAtualizado = carroselItemTranslateXAtual + carroselItemTranslateXAjuste;
-        carroselItemTranslateXAtualizado = limiteArrastandoAtingido ? carroselItemTranslateXAtualizado - (carroselItemLargura + carrosselItensGap) : carroselItemTranslateXAtualizado;
+        carroselItemTranslateXAtualizado = carroselItemTranslateXAjuste;
     }
 
-    if (!carroselItemTranslateXAtual) {
+    if (!carroselItemTranslateXAtual) {//se for 0px não faz nada
         carroselItemTranslateXAtualizado = carroselItemTranslateXAtual;
+    } else {
+        verificarBotoes(carrosselLista, carroselItemTranslateXAtual, carrosselItensGap);
     }
+
 
     const carroselItemAnimacaoMagneticaDuracao = 300;
     carrosselItens.css({
@@ -95,6 +105,39 @@ $(document).on('mouseup touchend', function (evento) {
 
         animandoItem = false;
     }, carroselItemAnimacaoMagneticaDuracao);
+}
+// const arrastarCarroselInfinitoItem = () => {
+
+// }
+// const arrastarCarroselFinitoBlocoItem = () => {
+
+// }
+// const arrastarCarroselInfinitoBlocoItem = () => {
+
+// }
+
+
+carrosseisArrastaveisLista.on('mousedown touchstart', function (evento) {
+    if (evento.target.classList.contains('carrossel-customizado-lista')) return;
+    if (animandoItem) return;
+    arrastando = true;
+
+    const carrosselLista = $(evento.currentTarget);
+    carrosselLista.next('.carrossel-customizado-btn-proximo.arrastavel').addClass('arrastando');
+    carrosselLista.find('.carrossel-customizado-item').css('transition', 'none');
+
+    posicaoInicialEixoX = (evento.pageX || evento.originalEvent.touches[0].pageX) - carrosselLista.offset().left;
+    posicaoScrollAtual = carrosselLista.scrollLeft();
+});
+
+$(document).on('mouseup touchend', function (evento) {
+    if (!arrastando) return;
+    arrastando = false;
+    animandoItem = true;
+
+    const containerAtual = $(evento.currentTarget).find('.carrossel-customizado-btn-proximo.arrastavel.arrastando').closest('.carrossel-customizado-container');
+
+    arrastarCarroselFinitoItem(containerAtual);
 });
 
 carrosseisArrastaveisLista.on('mousemove touchmove', function (evento) {
